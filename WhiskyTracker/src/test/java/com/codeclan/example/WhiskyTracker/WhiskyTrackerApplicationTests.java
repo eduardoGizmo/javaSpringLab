a184e6994910023709eaaf86002aa9062dbab6e6
Talbekk <mt.campbell@outlook.com>
2020-01-22 15:43:04 +0000

    adds second test

--- WhiskyTracker/src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ WhiskyTracker/src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,6 +1,8 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,12 +13,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
 
 	@Autowired
 	WhiskyRepository whiskyRepository;
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
 
 	@Test
 	public void contextLoads() {
@@ -25,9 +32,15 @@ public class WhiskyTrackerApplicationTests {
 	@Test
 	public void canFindWhiskyByYear(){
 		List<Whisky> found = whiskyRepository.findWhiskyByYear(1991);
-		Assert.assertEquals(1991, found.get(0).getYear());
+		assertEquals(1991, found.get(0).getYear());
 	}
 
-	
+	@Test
+	public void canFindDistilleriesThatHaveRegionName(){
+		List<Distillery> found = distilleryRepository.findDistilleriesByRegion("Lowland");
+		assertEquals("Rosebank", found.get(0).getName());
+	}
+
+
 
 }
